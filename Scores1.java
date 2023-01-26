eb4d6030bb7dfe10b7c3764cb91cef3f34961624
minasaic <smn96y@gmail.com>
2023-01-26 17:31:18 +0900

    first java01

---
+++ Scores1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+class Scores {
+
+	public static void main(String[] args) {
+	try(BufferedReader reader= Files.newBufferedReader(
+				Paths.get(".","java-kadai01","scores.csv"))){
+					String line =" ";
+					while((line = reader.readLine()) != null){
+						System.out.println(line);
+					}
+					reader.close();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+	}
+}
